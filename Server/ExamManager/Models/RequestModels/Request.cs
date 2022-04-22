a2d41c3aed62b431843fde5592c8fbb1293a993c
Ilya Nechaev <ilia.nechaeff@yandex.ru>
2022-04-23 01:41:00 +0300

    Реализовать структуру тел запросов и ответов API

--- Server/ExamManager/Models/RequestModels/Request.cs
+++ Server/ExamManager/Models/RequestModels/Request.cs
@@ -1,12 +1,74 @@
 ﻿namespace ExamManager.Models.RequestModels;
 
+public struct CreateTaskRequest
+{
+    public string title { get; set; }
+    public string description { get; set; }
+    public string url { get; set; }
+    public Guid studentId { get; set; }
+    public Guid authorId { get; set; }
+}
+
+public struct DeleteTaskRequest
+{
+    public Guid taskId { get; set; }
+}
+
+public struct ModifyTaskRequest
+{
+    public Guid taskId { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public Guid studentId { get; set; }
+    public Guid authorId { get; set; }
+    public TaskStatus status { get; set; }
+}
+
 public struct CreateGroupRequest
 {
     public string GroupName { get; set; }
+}
+
+public struct AddStudentsRequest
+{
+    public Guid groupId { get; set; }
+    public StudentView[] students { get; set; }
+    public struct StudentView
+    {
+        public Guid id { get; set; }
+    }
+}
+
+public struct RemoveStudentsRequest
+{
+    public Guid groupId { get; set; }
+    public StudentView[] students { get; set; }
+
+    public struct StudentView
+    {
+        public Guid studentId { get; set; }
+        public Guid groupId { get; set; }
+    }
+}
+
+public struct GetStudentsRequest
+{
+    public Guid groupId { get; set; }
+    public TaskStatus taskStatus { get; set; }
 }
 
 public struct CreateStudentsRequest
 {
     public Guid GroupId { get; set; }
     public List<RegisterEditModel> Students { get; set; }
+}
+
+public struct DeleteStudentsRequest
+{
+    public StudentView[] students { get; set; }
+    public struct StudentView
+    {
+        public Guid id { get; set; }
+        public bool onlyLogin { get; set; }
+    }
 }
--- Server/ExamManager/Models/RequestModels/Response.cs
+++ Server/ExamManager/Models/RequestModels/Response.cs
@@ -2,35 +2,75 @@
 
 namespace ExamManager.Models.Response;
 
-public class Response
+public record Response
 {
     public HttpStatusCode status { get; set; }
     public string type { get => GetType().Name; }
 }
 
-public class BadResponse : Response
-{
-    public Dictionary<string, List<string>>? errors { get; set; }
-}
-
-public class JWTResponse : Response
+public record JWTResponse : Response
 {
     public string token { get; set; }
 }
 
-public class ExceptionResponse : Response
+public record UserDataResponse : Response
+{
+    public Guid id{ get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public UserRole role { get; set; }
+}
+
+public record UsersDataResponse : Response
+{
+    public List<UserView> users { get; set; }
+    public struct UserView
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string groupName { get; set; }
+    }
+}
+
+public record TasksDataResponse : Response
+{
+    public TaskView[] tasks { get; set; }
+    public struct TaskView
+    {
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public TaskStatus status { get; set; }
+        public Guid studentId { get; set; }
+    }
+}
+
+public record TaskDataResponse : Response
+{
+    public Guid id { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public TaskStatus status { get; set; }
+    public Guid authorId { get; set; }
+    public string url { get; set; }
+}
+
+public record GroupDataResponse : Response
+{
+    public Guid id { get; set; }
+    public string name { get; set; }
+    public int studentsCount { get; set; }
+}
+
+public record BadResponse : Response
+{
+    public Dictionary<string, List<string>>? errors { get; set; }
+}
+
+public record ExceptionResponse : Response
 {
     public string exceptionType { get; set; }
     public string message { get; set; }
     public string stackTrace { get; set; }
-}
-
-public class UserDataResponse : Response
-{
-    public UserViewModel data { get; set; }
-}
-
-public class GroupDataResponse : Response
-{
-    public Group data { get; set; }
 }
