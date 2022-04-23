3188db2ab586a1518de8dd45e08e5da694ba9b1a
Ilya Nechaev <ilia.nechaeff@yandex.ru>
2022-04-23 17:46:31 +0300

    Реализовать сервис IStudentTaskService

---
+++ Server/ExamManager/Services/Implementations/StudentTaskService.cs
@@ -0,0 +1,57 @@
+﻿using ExamManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamManager.Services.Implementations;
+
+
+public class StudentTaskService : IStudentTaskService
+{
+    DbContext _dbContext;
+    public StudentTaskService(DbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public async Task<StudentTask> ChangeStudentTaskStatus(Guid taskId, StudentTask.TaskStatus status)
+    {
+        var StudentTaskSet = _dbContext.Set<StudentTask>();
+
+        var studentTask = await StudentTaskSet.FirstOrDefaultAsync(t => t.ObjectID == taskId);
+
+        studentTask.Status = status;
+        //StudentTaskSet.Update(studentTask);
+        await _dbContext.SaveChangesAsync();
+
+        return studentTask;
+    }
+
+    public async Task<StudentTask> CreateStudentTask(StudentTask task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudentTask> CreateStudentTask(string title, string description, string url, Guid authorId, Guid studentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteStudentTask(Guid taskId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudentTask> GetStudentTask(Guid taskId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<StudentTask>> GetStudentTasks(Guid studentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StudentTask> UpdateStudentTask(StudentTask newTask, Guid? taskId = null)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Server/ExamManager/Services/Interfaces/IStudentTaskService.cs
@@ -0,0 +1,14 @@
+﻿using ExamManager.Models;
+
+namespace ExamManager.Services;
+
+public interface IStudentTaskService
+{
+    public Task<StudentTask> GetStudentTask(Guid taskId);
+    public Task<IEnumerable<StudentTask>> GetStudentTasks(Guid studentId);
+    public Task<StudentTask> CreateStudentTask(StudentTask task);
+    public Task<StudentTask> CreateStudentTask(string title, string description, string url, Guid authorId, Guid studentId);
+    public Task<StudentTask> ChangeStudentTaskStatus(Guid taskId, StudentTask.TaskStatus status);
+    public Task<StudentTask> UpdateStudentTask(StudentTask newTask, Guid? taskId = null);
+    public Task DeleteStudentTask(Guid taskId);
+}
