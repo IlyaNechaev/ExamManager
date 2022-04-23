13cbb37ab4c13400d4472b327b6e8222bd234da9
Ilya Nechaev <ilia.nechaeff@yandex.ru>
2022-04-23 17:46:01 +0300

    Добавить сущность StudentTask

--- Server/ExamManager/Models/StudentTask.cs
+++ Server/ExamManager/Models/StudentTask.cs
@@ -1,6 +1,27 @@
-﻿namespace ExamManager.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExamManager.Models;
+
+public record StudentTask
 {
-    public record StudentTask
+    public Guid ObjectID { get; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public StudentTask.TaskStatus Status { get; set; }
+    public string Url { get; set; }
+
+    [ForeignKey(nameof(StudentID))]
+    public User Student { get; set; }
+    public Guid StudentID { get; set; }
+
+    [ForeignKey(nameof(AuthorID))]
+    public User Author { get; set; }
+    public Guid AuthorID { get; set; }
+
+    public enum TaskStatus
     {
+        CREATED,
+        FAILED,
+        SUCCESSED
     }
 }
