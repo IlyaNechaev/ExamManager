7e6c9e94b4f6cdb0f0d1a130c468368d2890b306
Ilya Nechaev <inechaev@croc.ru>
2022-05-21 01:05:16 +0300

    Добавить Mapper запросов

---
+++ Server/ExamManager/Mapping/RequestsMapping.cs
@@ -0,0 +1,37 @@
+﻿using ExamManager.Models;
+using ExamManager.Models.RequestModels;
+
+namespace ExamManager.Mapping;
+
+
+public partial class MappingProfile
+{
+    [PerformMapping]
+    private void RequestsMapping()
+    {
+        CreateMap<CreateTaskRequest, StudyTask>()
+            .ForMember(task => task.Title, options => options.MapFrom(request => request.title))
+            .ForMember(task => task.Description, options => options.MapFrom(request => request.description))
+            .ForMember(task => task.VirtualMachines, options => options.MapFrom(request => MapVirtualMachines(request.virtualMachines)))
+            ;
+
+        // CreateUsersRequest -> IEnumerable<User>
+        CreateMap<CreateUsersRequest.UserView, User>()
+            .ForMember(user => user.FirstName, options => options.MapFrom(view => view.firstName))
+            .ForMember(user => user.LastName, options => options.MapFrom(view => view.lastName))
+            .ForMember(user => user.Login, options => options.MapFrom(view => view.login))
+            .ForMember(user => user.PasswordHash, options => options.MapFrom(view => _securityService.Encrypt(view.password)))
+            ;
+
+    }
+
+    private VirtualMachineImage[] MapVirtualMachines(CreateTaskRequest.VirtualMachineView[] vmViews)
+    {
+        return vmViews.Select(view => new VirtualMachineImage
+        {
+            Title = view.title,
+            ID = view.id,
+            Order = view.order
+        }).ToArray();
+    }
+}
